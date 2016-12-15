79e09e175718a73a4ac7bbc183477c032eaf2d03
zhengzhi2936 <zhengzhi2616@gmail.com>
2016-12-15 19:52:39 +0800

    implement posts pagination with will_paginate

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,7 +9,7 @@ def new
  end
   def show
    @group = Group.find(params[:id])
-    @posts = @group.posts.recent
+    @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
  end
  def edit
 
