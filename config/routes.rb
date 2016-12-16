62da05c56302a305d5d83ab595fedfe855204366
zhengzhi2936 <zhengzhi2616@gmail.com>
2016-12-16 13:44:01 +0800

    implement my groups

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
       post :join
       post :quit
     end
+
     resources :posts
+  end
+  namespace :account do
+  resources :groups
   end
   root 'groups#index'
 end
