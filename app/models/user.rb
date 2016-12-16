8d928eeae99e75697b238ac2e02c8b4975b93d05
zhengzhi2936 <zhengzhi2616@gmail.com>
2016-12-16 13:28:53 +0800

    implement is_member_of?(group)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
+  def is_member_of?(group)
+    participated_groups.include?(group)
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
            has_many :groups
              has_many :posts
              has_many :group_relationships
